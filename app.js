7952214925c5bce73ada95ce5632d9c6ab915c4e
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-12 12:01:33 -0700

    Implement grocery list

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // TODO
+// 
+
+let GroceryListItem = (props) => (
+  <li>{props.item}</li>
+)
+
+const GroceryList = (props) => (
+
+<ul>
+  {props.items.map(item => <GroceryListItem item={item}/>)}
+</ul>
+);
+
+const App = () => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryList items={['burrito', 'pizza', 'chocolate']}/>
+  </div>
+);
+
+
+
+// // Here we create a `TodoList` component
+// var TodoList = (props) => (
+//   <ul>
+//     <li>{props.todos[0]}</li>
+//     <li>{props.todos[1]}</li>
+//     <li>{props.todos[2]}</li>
+//   </ul>
+// );
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// );
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
